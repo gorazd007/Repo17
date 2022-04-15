2c524fd01b5411552fd052836fdf448265d1c79e
gorazd007 <gorazdtezak@gmail.com>
2022-04-16 01:11:00 +0200

    correction

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -3,12 +3,12 @@ const BAKING_TEMPERATURES = {
     cake: { low: 180, high: 250 },
     bread: { low: 150, high: 220 },
     pie: { low: 200, high: 230 }
-};
+}
 
 function main(baking) {
 
-    const
-    const 
+    const { bread: {low: lowBread, high: highBread }} = BAKING_TEMPERATURES
+
 
     return { lowBread, highBread };
 
